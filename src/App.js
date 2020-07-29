aa74ef41d4198727045f0493f23e3a794c745308
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-07-30 00:52:21 +0530

    APP.JS: Imported Routes file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Routes from "./routes";
 
 function App() {
-  return <div className="App">Hemant</div>;
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
 }
 
 export default App;
