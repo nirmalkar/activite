55d282eb121f676c2275578c8d3649007da807c3
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-07-30 01:01:33 +0530

    COMPONENT-DASHBOARDCONTENT: created and imported dashboardContentRoutes inside it, User list component created

---
+++ src/components/dashboardContent/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Layout } from "antd";
+
+import dashboardContentRoutes from "../../routes/dashboardContentRoutes";
+const DashboardContent = () => {
+  const { Content } = Layout;
+  return (
+    <Content>
+      <dashboardContentRoutes />
+    </Content>
+  );
+};
+
+export default DashboardContent;
